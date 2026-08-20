1ac75ccd1cb681c4bba8eb2a84e3d42d50417d78
BijanProgrammer <bijaneisapour@gmail.com>
2026-08-20 12:08:47 +0330

    [CI/CD][UPDATE] output to export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/tuag',
-    output: 'standalone',
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
